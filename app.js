b3646f0d665cadf5e7ed52a65fd8a34c050791c2
Deepanshu40 <gargdeepanshu40@gmail.com>
2024-05-27 05:20:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const Queue = require('bull')
+const app = express();
+
+const port = 3000;
+const myQueue = new Queue('myQueue', 'redis://127.0.0.1:6379');
+
+
+// async function runQueue() {
+// await new Promise(resolve => setTimeout(resolve, 5000));
+
+// }
+
+for (let i=0; i<=50; i++) {
+    // runQueue();
+    myQueue.add({name: `task ${i}`});
+
+}
+
+
+myQueue.process(1, async(job) => {
+
+await new Promise(resolve => setTimeout(resolve, 5000));
+// console.log(job);
+console.log(`Process job has been completed successfully: ${job.data.name}`);
+
+})
+
+app.listen(port, () => {
+    console.log('app is listening');
+})
+
